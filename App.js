89d81f98079890831313f5513338b153c7fa632f
JViniciusF <jose.santana20@fatec.sp.gov.br>
2020-10-05 19:44:11 -0300

    TESTES , nada funcional

--- App.js
+++ App.js
@@ -1,21 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import SideMenu from 'react-native-side-menu'
 
-export default function App() {
+class SideBar {
+  render(){
+    return (
+      <View style={[ styles.container, { backgroundColor: '#fff' } ]}>
+        <Text> 
+        <Icon name="rocket" size={30} color="#900" />
+        Conteúdo side bar
+        </Text>
+      </View>
+    );
+  } 
+};
+
+function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Home!</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function SettingsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Settings!</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+
+function MyTabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Settings" component={SettingsScreen} />
+    </Tab.Navigator>
+  );
+}
+function LeftTab(){
+  return(
+    <SideMenu name = "sideMenu" component={SideBar}>
+    </SideMenu>
+  )
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <MyTabs/>
+      <LeftTab/>
+
+    </NavigationContainer>
+  );
+}
+
